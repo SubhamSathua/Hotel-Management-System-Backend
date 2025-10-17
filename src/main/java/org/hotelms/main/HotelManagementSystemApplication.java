1914cde5a7029a38febdb2c6441a6fa06fdb731e
Subham Kumar Sathua <137478528+SubhamSathua@users.noreply.github.com>
2025-10-17 22:31:31 +0530

    Created: basic login backend

--- src/main/java/org/hotelms/main/HotelManagementSystemApplication.java
+++ src/main/java/org/hotelms/main/HotelManagementSystemApplication.java
@@ -1,4 +1,4 @@
-package org.hotelms.backend;
+package org.hotelms.main;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/org/hotelms/main/HotelManagementSystemApplicationTests.java
+++ src/test/java/org/hotelms/main/HotelManagementSystemApplicationTests.java
@@ -1,4 +1,4 @@
-package org.hotelms.backend;
+package org.hotelms.main;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
